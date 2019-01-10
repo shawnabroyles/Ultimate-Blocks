2d1b1981d087c2c9e17a94ea92c21662c0aa8686
Alexius08 <jrsfac@gmail.com>
2019-01-10 19:55:20 +0800

    Make circular-style countdown

--- src/blocks/countdown/block.js
+++ src/blocks/countdown/block.js
@@ -1,11 +1,15 @@
-import icon from './icon';
+import icon, { RegularCountdownIcon, CircularCountdownIcon } from './icon';
 
 import { Component } from 'react';
 
+import Circle from './CircularCountdown';
+
+import './style.scss';
+
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { InspectorControls } = wp.editor;
-const { DateTimePicker } = wp.components;
+const { InspectorControls, BlockControls } = wp.editor;
+const { DateTimePicker, Toolbar, Button } = wp.components;
 
 class Timer extends Component {
 	constructor(props) {
@@ -13,10 +17,10 @@ class Timer extends Component {
 		this.state = { timeLeft: this.remainingTime() };
 	}
 	remainingTime = () => {
-		return Math.ceil((Date.parse(this.props.deadline) - Date.now()) / 1000);
+		return this.props.deadline - Math.floor(Date.now() / 1000);
 	};
 	componentDidMount() {
-		this.tick = setInterval(this.ticker, 1000);
+		this.tick = setInterval(this.ticker, 1000); //debugging
 	}
 	ticker = () => {
 		this.setState({
@@ -27,9 +31,7 @@ class Timer extends Component {
 		if (newProps.deadline !== this.props.deadline) {
 			clearInterval(this.tick);
 			this.setState({
-				timeLeft: Math.ceil(
-					(Date.parse(newProps.deadline) - Date.now()) / 1000
-				)
+				timeLeft: newProps.deadline - Math.floor(Date.now() / 1000)
 			});
 			this.tick = setInterval(this.ticker, 1000);
 		}
@@ -45,8 +47,46 @@ class Timer extends Component {
 		const seconds = timeLeft % 60;
 		const minutes = ((timeLeft - seconds) % 3600) / 60;
 		const hours = ((timeLeft - minutes * 60 - seconds) % 86400) / 3600;
-		const days = (timeLeft - hours * 3600 - minutes * 60 - seconds) / 86400;
-		return <p>{`${days}:${hours}:${minutes}:${seconds}`}</p>;
+		const days =
+			((timeLeft - hours * 3600 - minutes * 60 - seconds) % 604800) /
+			86400;
+		const weeks =
+			(timeLeft - days * 86400 - hours * 3600 - minutes * 60 - seconds) /
+			604800;
+
+		const defaultFormat = (
+			<p>{`${weeks}:${days}:${hours}:${minutes}:${seconds}`}</p>
+		);
+
+		const circularFormat = (
+			<div className="ub_countdown_circular_container">
+				<Circle amount={weeks} total={52} />
+				<Circle amount={days} total={7} />
+				<Circle amount={hours} total={24} />
+				<Circle amount={minutes} total={60} />
+				<Circle amount={seconds} total={60} />
+				<p>Weeks</p>
+				<p>Days</p>
+				<p>Hours</p>
+				<p>Minutes</p>
+				<p>Seconds</p>
+				{timeLeft <= 0 && <p>Time's up!</p>}
+			</div>
+		);
+
+		let selectedFormat;
+
+		switch (this.props.timerStyle) {
+			case 'Regular':
+				selectedFormat = defaultFormat;
+				break;
+			case 'Circular':
+				selectedFormat = circularFormat;
+				break;
+			default:
+				selectedFormat = defaultFormat;
+		}
+		return selectedFormat;
 	}
 }
 
@@ -57,26 +97,49 @@ registerBlockType('ub/countdown', {
 	keywords: [__('Countdown'), __('Timer'), __('Ultimate Blocks')],
 	attributes: {
 		endDate: {
+			type: 'number',
+			default: 60000 * (1440 + Math.ceil(Date.now() / 60000)) // 24 hours from Date.now
+		},
+		style: {
 			type: 'string',
-			default: new Date(
-				60000 * (1440 + Math.ceil(Date.now() / 60000)) // 24 hours from Date.now
-			).toISOString() //should be unix time
+			default: 'Regular' //available types: Regular, Circular, Odometer
 		}
 	},
 
 	edit(props) {
 		const { isSelected, setAttributes } = props;
-		const { endDate } = props.attributes;
+		const { style, endDate } = props.attributes;
 		return [
+			isSelected && (
+				<BlockControls>
+					<Toolbar>
+						<Button
+							onClick={() => setAttributes({ style: 'Regular' })}
+						>
+							{RegularCountdownIcon}
+						</Button>
+						<Button
+							onClick={() => setAttributes({ style: 'Circular' })}
+						>
+							{CircularCountdownIcon}
+						</Button>
+					</Toolbar>
+				</BlockControls>
+			),
 			isSelected && (
 				<InspectorControls>
 					<DateTimePicker
-						currentDate={endDate}
-						onChange={value => setAttributes({ endDate: value })}
+						currentDate={endDate * 1000}
+						onChange={value => {
+							console.log(value);
+							setAttributes({
+								endDate: Math.floor(Date.parse(value) / 1000)
+							});
+						}}
 					/>
 				</InspectorControls>
 			),
-			<Timer deadline={endDate} />
+			<Timer timerStyle={style} deadline={endDate} />
 		];
 	},
 
